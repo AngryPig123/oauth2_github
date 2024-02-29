c5e1c2d30f2a52d361702778edfe9849ef428937
Angrypig123 <shguddnr3@naver.com>
2024-02-29 23:57:30 +0900

    OAuth2 적용

---
+++ src/main/java/org/spring/oauth2/config/security/ProjectConfig.java
@@ -0,0 +1,56 @@
+package org.spring.oauth2.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+//    oauth2:
+    //    github:
+    //    id: 74ec01050e2b8ca19a57
+    //    secret: 164827a52ffbc4b438431cf2b01413957b974234
+
+
+    @Value("${oauth2.github.id}")
+    private String id;
+
+    @Value("${oauth2.github.secret}")
+    private String secret;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //  OAuth2 사용 설정
+        http
+                .oauth2Login(client -> client
+                        .clientRegistrationRepository(clientRegistrationRepository())
+                );
+        //  모든 end-point 인증 설정
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+
+    private ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration clientRegistration = clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB
+                .getBuilder("github")
+                .clientId(id)
+                .clientSecret(secret)
+                .build();
+    }
+
+}
